1ce1f2eb53f51f6ed46a97c469f72fc4f297d3ec
parzip4l <sobirinjamo33@gmail.com>
2023-07-29 12:58:08 +0700

    new

--- resources/views/pages/user-pages/index.blade.php
+++ resources/views/pages/user-pages/index.blade.php
@@ -12,7 +12,7 @@
     <div class="container">
         <div class="content-welcome" style="text-white">
             <div class="col-md-6">
-                <h1 class="text-white mb-2">SELAMAT DATANG DI PORTAL CILELES SMART</h1>
+                <h1 class="text-white mb-2">SELAMAT DATANG DI PORTAL CILELES SMART sss</h1>
                 <p class="text-light">Lorem ipsum dolor sit amet consectetur. Diam diam nibh aliquam amet orci. Suspendisse massa posuere senectus tincidunt odio volutpat et tellus. Molestie purus arcu sed bibendum quam imperdiet etiam interdum nisl. Phasellus dapibus vulputate maecenas libero a consequat scelerisque etiam potenti. Feugiat mattis eget amet amet.</p>
             </div> 
         </div>
