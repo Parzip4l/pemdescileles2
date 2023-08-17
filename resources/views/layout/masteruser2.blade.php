79a877ebd7defe5e4989b1bf1beee09dc59910b9
parzip4l <sobirinjamo33@gmail.com>
2023-08-17 17:57:49 +0700

    update meta description

--- resources/views/layout/masteruser2.blade.php
+++ resources/views/layout/masteruser2.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <meta name="description" content="Cileles Smart Web Aps">
+  <meta name="description" content="Cileles Smart Web Apps || MELAJU BERMARTABAT MENUJU CILELES SMART">
 	<meta name="author" content="Rinable Creative">
 	<meta name="keywords" content="desacileles, cileles, smartvillages">
 
