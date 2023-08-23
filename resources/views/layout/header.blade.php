855d8bbed8fca41ca65cc6481ac6302c2bb77323
parzip4l <sobirinjamo33@gmail.com>
2023-08-23 16:46:45 +0700

    user profile

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -3,27 +3,7 @@
     <i data-feather="menu"></i>
   </a>
   <div class="navbar-content">
-    <form class="search-form">
-      <div class="input-group">
-        <div class="input-group-text">
-          <i data-feather="search"></i>
-        </div>
-        <input type="text" class="form-control" id="navbarForm" placeholder="Search here...">
-      </div>
-    </form>
     <ul class="navbar-nav">
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="languageDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <img src="{{ url('assets/images/flags/us.svg') }}" class="wd-20 me-1" title="us" alt="us"> <span class="ms-1 me-1 d-none d-md-inline-block">English</span>
-        </a>
-        <div class="dropdown-menu" aria-labelledby="languageDropdown">
-          <a href="javascript:;" class="dropdown-item py-2"> <img src="{{ url('assets/images/flags/us.svg') }}" class="wd-20 me-1" title="us" alt="us"> <span class="ms-1"> English </span></a>
-          <a href="javascript:;" class="dropdown-item py-2"> <img src="{{ url('assets/images/flags/fr.svg') }}" class="wd-20 me-1" title="fr" alt="fr"> <span class="ms-1"> French </span></a>
-          <a href="javascript:;" class="dropdown-item py-2"> <img src="{{ url('assets/images/flags/de.svg') }}" class="wd-20 me-1" title="de" alt="de"> <span class="ms-1"> German </span></a>
-          <a href="javascript:;" class="dropdown-item py-2"> <img src="{{ url('assets/images/flags/pt.svg') }}" class="wd-20 me-1" title="pt" alt="pt"> <span class="ms-1"> Portuguese </span></a>
-          <a href="javascript:;" class="dropdown-item py-2"> <img src="{{ url('assets/images/flags/es.svg') }}" class="wd-20 me-1" title="es" alt="es"> <span class="ms-1"> Spanish </span></a>
-        </div>
-      </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="appsDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           <i data-feather="grid"></i>
@@ -53,82 +33,6 @@
         </div>
       </li>
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="messageDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <i data-feather="mail"></i>
-        </a>
-        <div class="dropdown-menu p-0" aria-labelledby="messageDropdown">
-          <div class="px-3 py-2 d-flex align-items-center justify-content-between border-bottom">
-            <p>9 New Messages</p>
-            <a href="javascript:;" class="text-muted">Clear all</a>
-          </div>
-          <div class="p-1">
-            <a href="javascript:;" class="dropdown-item d-flex align-items-center py-2">
-              <div class="me-3">
-                <img class="wd-30 ht-30 rounded-circle" src="{{ url('https://via.placeholder.com/30x30') }}" alt="userr">
-              </div>
-              <div class="d-flex justify-content-between flex-grow-1">
-                <div class="me-4">
-                  <p>Leonardo Payne</p>
-                  <p class="tx-12 text-muted">Project status</p>
-                </div>
-                <p class="tx-12 text-muted">2 min ago</p>
-              </div>	
-            </a>
-            <a href="javascript:;" class="dropdown-item d-flex align-items-center py-2">
-              <div class="me-3">
-                <img class="wd-30 ht-30 rounded-circle" src="{{ url('https://via.placeholder.com/30x30') }}" alt="userr">
-              </div>
-              <div class="d-flex justify-content-between flex-grow-1">
-                <div class="me-4">
-                  <p>Carl Henson</p>
-                  <p class="tx-12 text-muted">Client meeting</p>
-                </div>
-                <p class="tx-12 text-muted">30 min ago</p>
-              </div>	
-            </a>
-            <a href="javascript:;" class="dropdown-item d-flex align-items-center py-2">
-              <div class="me-3">
-                <img class="wd-30 ht-30 rounded-circle" src="{{ url('https://via.placeholder.com/30x30') }}" alt="userr">
-              </div>
-              <div class="d-flex justify-content-between flex-grow-1">
-                <div class="me-4">
-                  <p>Jensen Combs</p>
-                  <p class="tx-12 text-muted">Project updates</p>
-                </div>
-                <p class="tx-12 text-muted">1 hrs ago</p>
-              </div>	
-            </a>
-            <a href="javascript:;" class="dropdown-item d-flex align-items-center py-2">
-              <div class="me-3">
-                <img class="wd-30 ht-30 rounded-circle" src="{{ url('https://via.placeholder.com/30x30') }}" alt="userr">
-              </div>
-              <div class="d-flex justify-content-between flex-grow-1">
-                <div class="me-4">
-                  <p>Amiah Burton</p>
-                  <p class="tx-12 text-muted">Project deatline</p>
-                </div>
-                <p class="tx-12 text-muted">2 hrs ago</p>
-              </div>	
-            </a>
-            <a href="javascript:;" class="dropdown-item d-flex align-items-center py-2">
-              <div class="me-3">
-                <img class="wd-30 ht-30 rounded-circle" src="{{ url('https://via.placeholder.com/30x30') }}" alt="userr">
-              </div>
-              <div class="d-flex justify-content-between flex-grow-1">
-                <div class="me-4">
-                  <p>Yaretzi Mayo</p>
-                  <p class="tx-12 text-muted">New record</p>
-                </div>
-                <p class="tx-12 text-muted">5 hrs ago</p>
-              </div>	
-            </a>
-          </div>
-          <div class="px-3 py-2 d-flex align-items-center justify-content-center border-top">
-            <a href="javascript:;">View all</a>
-          </div>
-        </div>
-      </li>
-      <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="notificationDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           <i data-feather="bell"></i>
           <div class="indicator">
@@ -137,7 +41,7 @@
         </a>
         <div class="dropdown-menu p-0" aria-labelledby="notificationDropdown">
           <div class="px-3 py-2 d-flex align-items-center justify-content-between border-bottom">
-            <p>6 New Notifications</p>
+            <p>Notifications</p>
             <a href="javascript:;" class="text-muted">Clear all</a>
           </div>
           <div class="p-1">
@@ -146,44 +50,8 @@
                 <i class="icon-sm text-white" data-feather="gift"></i>
               </div>
               <div class="flex-grow-1 me-2">
-                <p>New Order Recieved</p>
+                <p>Pengajuan Baru</p>
                 <p class="tx-12 text-muted">30 min ago</p>
-              </div>	
-            </a>
-            <a href="javascript:;" class="dropdown-item d-flex align-items-center py-2">
-              <div class="wd-30 ht-30 d-flex align-items-center justify-content-center bg-primary rounded-circle me-3">
-                <i class="icon-sm text-white" data-feather="alert-circle"></i>
-              </div>
-              <div class="flex-grow-1 me-2">
-                <p>Server Limit Reached!</p>
-                <p class="tx-12 text-muted">1 hrs ago</p>
-              </div>	
-            </a>
-            <a href="javascript:;" class="dropdown-item d-flex align-items-center py-2">
-              <div class="wd-30 ht-30 d-flex align-items-center justify-content-center bg-primary rounded-circle me-3">
-                <img class="wd-30 ht-30 rounded-circle" src="{{ url('https://via.placeholder.com/30x30') }}" alt="userr">
-              </div>
-              <div class="flex-grow-1 me-2">
-                <p>New customer registered</p>
-                <p class="tx-12 text-muted">2 sec ago</p>
-              </div>	
-            </a>
-            <a href="javascript:;" class="dropdown-item d-flex align-items-center py-2">
-              <div class="wd-30 ht-30 d-flex align-items-center justify-content-center bg-primary rounded-circle me-3">
-                <i class="icon-sm text-white" data-feather="layers"></i>
-              </div>
-              <div class="flex-grow-1 me-2">
-                <p>Apps are ready for update</p>
-                <p class="tx-12 text-muted">5 hrs ago</p>
-              </div>	
-            </a>
-            <a href="javascript:;" class="dropdown-item d-flex align-items-center py-2">
-              <div class="wd-30 ht-30 d-flex align-items-center justify-content-center bg-primary rounded-circle me-3">
-                <i class="icon-sm text-white" data-feather="download"></i>
-              </div>
-              <div class="flex-grow-1 me-2">
-                <p>Download completed</p>
-                <p class="tx-12 text-muted">6 hrs ago</p>
               </div>	
             </a>
           </div>
@@ -194,12 +62,12 @@
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="profileDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <img class="wd-30 ht-30 rounded-circle" src="{{ url('https://via.placeholder.com/30x30') }}" alt="profile">
+          <img class="wd-30 ht-30 rounded-circle" src="{{ asset('assets/images/user-profile.png') }}" alt="profile">
         </a>
         <div class="dropdown-menu p-0" aria-labelledby="profileDropdown">
           <div class="d-flex flex-column align-items-center border-bottom px-5 py-3">
             <div class="mb-3">
-              <img class="wd-80 ht-80 rounded-circle" src="{{ url('https://via.placeholder.com/80x80') }}" alt="">
+              <img class="wd-80 ht-80 rounded-circle" src="{{ asset('assets/images/user-profile.png') }}" alt="">
             </div>
             @if(Auth::check())
             <div class="text-center">
